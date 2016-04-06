ecbb99fb79b89e4cbcec107d242f6a9e4ae4b47e
forbesgrahamdce <forbes_graham@harvard.edu>
2016-04-06 10:10:47 -0400

    Adding methods

--- PitchClassSet.php
+++ PitchClassSet.php
@@ -1,5 +1,34 @@
 <?php
 class PitchClassSet implements Iterator {
+  protected $set = array();
+
+  public function __construct($set) {
+    $this->set = array_fill(0, 12, null);
+    foreach ($set as $element) {
+      $this->set[$element] = $element;
+    }
+  }
+
+  public function add($elements) {
+
+  }
+
+  public function remove($elements) {
+
+  }
+
+  public function complement() {
+
+  }
+
+  public function inversion() {
+
+  }
+
+  public function prime_form() {
+
+  }
+
   public function current() {
   
   }
