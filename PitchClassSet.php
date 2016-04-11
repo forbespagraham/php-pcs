f0ea51ed59603d1ac93298939137ae6921580d76
forbesgraham <blaqlghtn@gmail.com>
2016-04-11 07:27:21 -0400

    Working on prime form method

--- PitchClassSet.php
+++ PitchClassSet.php
@@ -4,6 +4,7 @@ class PitchClassSet implements Iterator {
   protected $set = array();
   public $rotations = array();
   static $distance_place = null;
+  static $rotations_copy = array();
   
   public function __construct($set) {
     $this->position = 0;
@@ -44,6 +45,18 @@ class PitchClassSet implements Iterator {
     return $r;
   }
 
+  public function generate_rotations($members) {
+	$r = array();
+    $count = count($members);
+    $r[] = $members;
+    for ($i = 1; $i < $count; $i++) {
+      $e = array_shift($members);
+      array_push($members, $e + 12);
+      $r[] = $members;
+    }
+    return $r;
+  }
+
   public function members() {
     $m = iterator_to_array($this);
     return $m;
@@ -63,35 +76,67 @@ class PitchClassSet implements Iterator {
   }
 
   public function inversion() {
-    $i = array();
     $members = $this->members();
-    foreach ($members as $element) {
-      $i[] = (12 - $element) % 12;
-    }
+    $i = $this->get_inversion($members);
     return $i;
   }
 
+  private function get_inversion($members) {
+	$i = array();
+	foreach ($members as $element) {
+      $i[] = (12 - $element) % 12;
+    }
+	return $i;
+  }
+
   public function prime_form() {
-    if (isset($distance_place)) {
-	  $distance_array = array();	  
+	$rotations_local_copy = array();
+    if (isset($this::$distance_place)) {
+	  $distance_array = array();
+	  foreach ($this::$rotations_copy as $rotation) {
+	    $distance_array[] = $this->distance($rotation);	
+	  }	  
 	}
 	else {
-	  $distance_place = count($this->members());
-	}
-	foreach ($this->rotations as $rotation) {
-      $distance_array[] = $this->distance($rotation);
+	  $this::$distance_place = count($this->members());
+	  $this::$rotations_copy = $this->rotations;
+	  foreach ($this->rotations as $rotation) {
+        $distance_array[] = $this->distance($rotation);
+	  }
 	}
 	if ($this->one_smallest($distance_array)) {
+	  $packed_array = array();
+	  $packed_smallest = 11;
 	  $smallest = $this->rotations[$this->smallest($distance_array)];
-	  return $this->normalize($smallest);	
+	  $normal = $this->normalize($smallest);
+	  $normal_inversion = $this->get_inversion($normal);
+	  $normal_rotations = $this->generate_rotations($normal_inversion);
+	  foreach ($normal_rotations as $key => $normal_rotation) { 
+	    $packed_array[] = array_shift($normal_rotation);
+	    $packed_array[] = array_shift($normal_rotation);
+	    $packed_distance = $packed_array[1] - $packed_array[0];
+	    if ($packed_distance < $packed_smallest) {
+		  
+	    }
+	  }
 	}
 	else {
-		
+	  $this::$distance_place--;
+	  foreach ($this::$rotations_copy as $rotation) {
+	    array_pop($rotation);
+	    $rotations_local_copy[] = $rotation;	
+	  }
+	  $this::$rotations_copy = $rotations_local_copy;
+	  return $this->prime_form();
 	}
   }
 
+  private function packed($distance_array) {
+	
+  }
+
   private function normalize($set) {
-	$normalized = array();
+    $normalized = array();
     $subtract_amt = array_shift($set);
     array_unshift($set, $subtract_amt);
     foreach ($set as $element) {
